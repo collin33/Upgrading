83c8385c4c7a72eb749b5a6b806dd77a3d83645b
TheProgram587 <0318428@student.rocvantwente.nl>
2020-09-03 11:37:15 +0200

    thrown item
    thrown item

---
+++ Assets/Thrown_item.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrown_item : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collision");
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("destory");
+            Destroy(gameObject);
+        }
+    }
+}
