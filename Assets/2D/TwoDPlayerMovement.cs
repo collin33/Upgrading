6abcd4aff006ffdc7c57eefe219edfde0bcb0f25
BuildTools <unconfigured@null.spigotmc.org>
2020-09-03 11:51:55 +0200

    2D Update

--- Assets/2D/TwoDPlayerMovement.cs
+++ Assets/2D/TwoDPlayerMovement.cs
@@ -7,41 +7,40 @@ public class TwoDPlayerMovement : MonoBehaviour
     Rigidbody2D rb;
     public float speed;
     public float jumpForce;
-    bool isGrounded = false;
-    int jumpsLeft;
-    public int jumpAmount;
+    public bool isGrounded = false;
     public float checkRadius;
     public LayerMask whatIsGround;
     public Transform GroundCheck;
+    public SpriteRenderer sprite;
+    public bool flipped;
 
     public void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     public void FixedUpdate()
     {
+        if (flipped) sprite.flipX = true; else sprite.flipX = false;
         isGrounded = Physics2D.OverlapCircle(GroundCheck.position, checkRadius, whatIsGround);
 
         //Movement from left to right
         if (Input.GetKey(KeyCode.A))
         {
             rb.velocity = new Vector2(-1 * speed, rb.velocity.y);
+            flipped = true;
         } else if (Input.GetKey(KeyCode.D))
         {
             rb.velocity = new Vector2(1 * speed, rb.velocity.y);
+            flipped = false;
         } else
         {
             rb.velocity = new Vector2(0, rb.velocity.y);
         }
 
-        //Jumping
-        if (isGrounded && rb.velocity.y == 0)
-        {
-            jumpsLeft = jumpAmount;
-        }
 
-        if (Input.GetKeyDown(KeyCode.Space) && jumpsLeft > 0)
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
             rb.velocity = Vector2.up * jumpForce;
         }
